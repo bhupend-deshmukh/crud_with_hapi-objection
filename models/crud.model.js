94eb6d0dc14b2cefa933b176ab1ae3ed13ef7003
Bhupendra Deshmukh <87089425+bhupend-deshmukh@users.noreply.github.com>
2022-10-07 15:26:34 +0530

    Update crud.model.js
    changes

--- models/crud.model.js
+++ models/crud.model.js
@@ -1,3 +1,4 @@
+const Joi = require('@hapi/joi')
 const { Model } = require("objection")
 const db = require("../config/db.connection")
 Model.knex(db)
@@ -10,9 +11,11 @@ class Userdata extends Model {
     static get JoiSchema() {
         return Joi.object({
             id: Joi.number().integer().greater(0),
-            name: Joi.string().min(1).required(),
-            email: Joi.string().email().max(100).required(),
-            password: Joi.string().min(8).required()
+            Name: Joi.string().min(1).max(50).required(),
+            Surname: Joi.string().min(1).max(50).required(),
+            email: Joi.max(50).required(),
+            password: Joi.string().min(5).required(),
+            city: Joi.string().required()
         })
     }
 }
